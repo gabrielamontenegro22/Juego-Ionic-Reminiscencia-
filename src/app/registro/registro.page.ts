2345e58c2eb09a29eb2bcdb60f655ac04a56e8b1
camilo <camilo_acostahe@fet.edu.com>
2024-08-26 21:34:20 -0500

    Subir Juego

---
+++ src/app/registro/registro.page.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.page.html',
+  styleUrls: ['./registro.page.scss'],
+})
+export class RegistroPage implements OnInit {
+
+  formularioRegistro: FormGroup;
+
+  constructor(public fb: FormBuilder) {
+    this.formularioRegistro = this.fb.group({
+      'nombre': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required),
+      'confirmacionPassword': new FormControl("", Validators.required)
+    });
+   }
+
+  ngOnInit() {
+  }
+
+  guardar(){
+    var f = this.formularioRegistro.value;
+
+    if (this.formularioRegistro.invalid){
+      
+    }
+  }
+
+}
