3dd3fe4976a0566e3e55906aa7f7c92b5f755afa
gabrielamontenegro22 <gabriela-montenegro22@hotmail.com>
2024-11-05 21:38:16 -0500

    Actualizaicon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,57 +10,7 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-<<<<<<< HEAD
-  },
-  {
-    path: 'registro',
-    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
-  },
-  {
-    path: 'componente',
-    loadChildren: () => import('./carpeta/componente/componente.module').then( m => m.ComponentePageModule)
-  },
-  {
-    path: 'inicio',
-    loadChildren: () => import('./inicio/inicio/inicio.module').then( m => m.InicioPageModule)
-  },
-  {
-    path: 'elige-rol',
-    loadChildren: () => import('./elige-rol/elige-rol.module').then( m => m.EligeRolPageModule)
-  },
-  {
-    path: 'salas',
-    loadChildren: () => import('./salas/salas.module').then( m => m.SalasPageModule)
-  },
-  {
-    path: 'captura-foto',
-    loadChildren: () => import('./captura-foto/captura-foto.module').then( m => m.CapturaFotoPageModule)
-  },
-  {
-    path: 'info-personaje',
-    loadChildren: () => import('./info-personaje/info-personaje.module').then( m => m.InfoPersonajePageModule)
-  },
-  {
-    path: 'prueba',
-    loadChildren: () => import('./prueba/prueba.module').then( m => m.PruebaPageModule)
-  },
-  {
-    path: 'amigos',
-    loadChildren: () => import('./amigos/amigos.module').then( m => m.AmigosPageModule)
-  },
-  {
-    path: 'barra-vida',
-    loadChildren: () => import('./barra-vida/barra-vida.module').then( m => m.BarraVidaPageModule)
-  },
-  {
-    path: 'ajuste',
-    loadChildren: () => import('./ajuste/ajuste.module').then( m => m.AjustePageModule)
-  },
-  {
-    path: 'student-crud',
-    loadChildren: () => import('./student-crud/student-crud.module').then( m => m.StudentCrudPageModule)
-=======
->>>>>>> DarwinMora
+
   },
   {
     path: 'registro',
