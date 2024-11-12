e692a0e0a7887be31d3bf50e23b911b93d0bc8b7
gabrielamontenegro22 <gabriela-montenegro22@hotmail.com>
2024-11-12 12:25:36 -0500

    Se agrego página restablecer contraseña 2 la cual solicita la nueva contraseña y la confirmacion

---
+++ src/app/restablecer2/restablecer2.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restablecer2',
+  templateUrl: './restablecer2.page.html',
+  styleUrls: ['./restablecer2.page.scss'],
+})
+export class Restablecer2Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
